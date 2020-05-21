613a3f78d9c30b191c79e527607e198bb98b7a80
Egor14 <egorgrekhnev@mail.ru>
2020-05-21 14:57:38 +0300

    new abi

--- gold_coin/abi.py
+++ gold_coin/abi.py
@@ -1,65 +1,55 @@
 json_abi = """
 [
     {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            },
-            {
-                "internalType": "uint256",
-                "name": "weight",
-                "type": "uint256"
-            },
-            {
-                "internalType": "string",
-                "name": "purchaseDate",
-                "type": "string"
-            },
-            {
-                "internalType": "string",
-                "name": "origin",
-                "type": "string"
-            },
-            {
-                "internalType": "string",
-                "name": "certifiedAssayer",
-                "type": "string"
-            },
-            {
-                "internalType": "bytes",
-                "name": "key",
-                "type": "bytes"
-            },
-            {
-                "internalType": "string",
-                "name": "pubKey",
-                "type": "string"
-            }
-        ],
-        "name": "_mint",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "weight",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "purchaseDate",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "origin",
+				"type": "string"
+			},
+			{
+				"internalType": "bytes",
+				"name": "key",
+				"type": "bytes"
+			}
+		],
+		"name": "_mint",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
 
     {
-        "inputs": [],
-        "name": "totalSupply",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ]"""
